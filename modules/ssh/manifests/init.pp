7a0ec3e06fd73c2331a306a2e394a50a1f4a5269
Aaron Root <atree@dragonrealm.net>
2015-03-22 01:50:29 -0400

    Enforces openssh

---
+++ modules/ssh/manifests/init.pp
@@ -0,0 +1,25 @@
+# ssh/manifests/init.pp
+class ssh {
+  $ssh_svc = $::osfamily ? {
+    Debian  => 'ssh',
+    Freebsd => 'sshd',
+    Default => fail("${module_name} does not support ${::osfamily}"),
+  }
+
+  if $::osfamily != 'FreeBSD' {
+    package { 'openssh-server':
+      ensure => latest,
+    }
+  }
+
+  file { '/etc/ssh/sshd_config':
+    ensure  => file,
+    content => template('ssh/sshd_config.erb'),
+  }
+
+  service { $svc:
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/ssh/sshd_config'],
+  }
+}
