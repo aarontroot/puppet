8b87aaab58f2d751db3a207cfe6fe2a33b7844a3
Aaron Root <atree@dragonrealm.net>
2015-03-23 12:45:05 -0400

    Users manifest with hiera example

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -1,3 +1,14 @@
+# Manage users via hiera
+#users:
+#  <username>:
+#    comment:    <real name>
+#    password:   <password hash>
+#    uid:        <User ID>
+#    shell:      <Path to shell>
+#    managehome: true
+#    groups:
+#      - <list of groups>
+
 class users {
   $users = hiera("users")
   create_resources(user, $users)
