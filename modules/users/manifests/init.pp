f657a871325acb1bccae87289f1b15b63effa117
Aaron Root <atree@lab-01.lab.dragonrealm.net>
2015-03-24 23:07:53 -0600

    Users module using hiera

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -1,3 +1,21 @@
+## Manage users via hiera
+## Example of hieradata file below.  Shell and groups also need
+## to be defined in hieradata/os/$::osfamily files
+## By: atree@dragonrealm.net
+
+# hieradata/users.yaml
+#---
+#users:
+# <username>:
+#   comment:    <real name>
+#   password:   <password hash>
+#   uid:        <User ID>
+#   managehome: true
+#   shell:      "%{hiera('<desired shell>')}"
+#   groups:
+#     -  "%{hiera('admingroup')}"
+
+
 class users {
   $users = hiera("users")
   create_resources(user, $users)
